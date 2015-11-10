df7c017ffe44958592d6bf2617c348b24ae401ef
hoffmaik <hoffmann.maik@gmail.com>
2015-11-10 09:16:31 +0100

    Methode für Knopf geschrieben..

--- testapp/testapp/Form1.cs
+++ testapp/testapp/Form1.cs
@@ -16,5 +16,10 @@ namespace testapp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hallo");
+        }
     }
 }
